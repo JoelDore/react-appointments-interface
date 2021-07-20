ea43abb4c459652416fa324b694504517379d072
JES20 <dore.joel.dore@gmail.com>
2021-07-20 13:33:33 -0400

    Add Search Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { BiArchive } from 'react-icons/bi'
+import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="App" >
-      <h1 style={{ display: 'flex', alignItems: 'flex-end', gap: '0.5rem' }}>
-        <BiArchive style={{ marginBottom: '0.2rem' }} />Your Appointments
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400 align-top" />
+        Your Appointments
       </h1>
+      <Search />
     </div>
   );
 }
